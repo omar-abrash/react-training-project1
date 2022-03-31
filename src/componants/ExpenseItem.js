a95318bf162243d416cd2315d286a4426b603b54
omar-abrash <omarabrash988@gmail.com>
2022-03-31 09:28:50 +0300

    change the hard data to Dynamic one

--- src/componants/ExpenseItem.js
+++ src/componants/ExpenseItem.js
@@ -1,12 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  // how to inject the variavles in JSX code :
+  const expenseDate = new Date("2022-03-31");
+  const expenseDescription = "Home Insurance";
+  const expenseAmount = 2500;
+  // after define the variables we can use {varName} between curly braces
   return (
     <div className="expense-item">
-      <div>30 March 2022</div>
+      <div>{expenseDate.toLocaleDateString()}</div>
       <div className="expense-item__description">
-        <h2>Home Insurance</h2>
-        <div className="expense-item__price">$3000</div>
+        <h2>{expenseDescription}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
