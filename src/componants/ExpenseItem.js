5304cc73cd091f762ade8a177c8337ec9d1a08a4
omar-abrash <omarabrash988@gmail.com>
2022-04-01 09:03:22 +0300

    split the  components to multiple components

--- src/componants/ExpenseItem.js
+++ src/componants/ExpenseItem.js
@@ -1,10 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   // we use props Parameter to acess the data from App.js
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
