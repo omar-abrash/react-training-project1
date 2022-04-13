e77ca6538642a9a0b7065d57d0a238ebb2c73b7c
omar-abrash <omarabrash988@gmail.com>
2022-04-13 05:14:05 +0300

    dynamic components adding

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import AllExpenseItems from "./componants/Expenses/AllExpenseItems";
-import React from "react";
-import NewExpense from "./componants/NewExpense/NewExpense";
+import AllExpenseItems from "./components/Expenses/AllExpenseItems";
+import React, { useState } from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 
+const dummy_expenses = [
+  {
+    id: "e1",
+    title: "Home Insurance",
+    date: new Date("2021-03-30"),
+    amount: 500,
+  },
+  {
+    id: "e2",
+    title: "Car Insurance",
+    date: new Date("2020-03-29"),
+    amount: 100,
+  },
+  {
+    id: "e3",
+    title: "Resturent Insurance",
+    date: new Date("2022-03-27"),
+    amount: 1000,
+  },
+  {
+    id: "e4",
+    title: "Helth Insurance",
+    date: new Date("2019-03-25"),
+    amount: 2500,
+  },
+];
 const App = () => {
   // if we want reCall the <ExpenseItem> with defferent Data we can use this array
-  const expenses = [
-    {
-      id: "e1",
-      title: "Home Insurance",
-      date: new Date("2021-03-30"),
-      amount: 500,
-    },
-    {
-      id: "e2",
-      title: "Car Insurance",
-      date: new Date("2020-03-29"),
-      amount: 100,
-    },
-    {
-      id: "e3",
-      title: "Resturent Insurance",
-      date: new Date("2022-03-27"),
-      amount: 1000,
-    },
-    {
-      id: "e4",
-      title: "Helth Insurance",
-      date: new Date("2019-03-25"),
-      amount: 2500,
-    },
-  ];
+  const [expenses, addNewExpense] = useState(dummy_expenses);
+
   const getCollectData = (expenseData) => {
-    const newExpenseData = {
-      ...expenseData,
-    };
-    console.log("In APP!!");
-    console.log(newExpenseData);
+    // console.log(expenseData);
+    addNewExpense((prevExpenses) => {
+      return [expenseData, ...prevExpenses];
+    });
   };
   // we must use attribute values to acces these data in this way :
   // look to ExpenseItem.js to see how acess the Extense array data to desired locations.
--- src/components/Expenses/AllExpenseItems.js
+++ src/components/Expenses/AllExpenseItems.js
@@ -20,26 +20,16 @@ const AllExpenseItems = (props) => {
   return (
     <Card className="expenses">
       <ExpensesFilter year={selectedYear} afterChangeYear={getNewYear} />
-      <ExpenseItem
-        title={props.expenses[0].title}
-        date={props.expenses[0].date}
-        amount={props.expenses[0].amount}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        date={props.expenses[1].date}
-        amount={props.expenses[1].amount}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        date={props.expenses[2].date}
-        amount={props.expenses[2].amount}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        date={props.expenses[3].date}
-        amount={props.expenses[3].amount}
-      />
+
+      {props.expenses.map((expense) => {
+        return (
+          <ExpenseItem
+            title={expense.title}
+            date={expense.date}
+            amount={expense.amount}
+          />
+        );
+      })}
     </Card>
   );
 };
